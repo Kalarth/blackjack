df1dd9bbd46532e98a66ad9386377e2bb77c7d0f
InesBaghou <33928466+InesBaghou@users.noreply.github.com>
2017-12-24 17:00:39 +0100

    Add files via upload
    bj.html : ajout des div pour changements d'images 
    bj.js : comptage des points, affichage des points a l'initialisation, ajout d'une carte pour la banque lorsque clique sur Reste. Probleme : mauvais comptage banque/joueur + le message de fin de partie ne s'affiche pas lorsque les points du joueur > 21

--- bj.js
+++ bj.js
@@ -1,11 +1,13 @@
+score_joueur = 0
+score_banque = 0
 
 var setupListeners = function(){
   var BoutonCarte = document.getElementById("carte");
   BoutonCarte.addEventListener("click",ajoutImgDansDiv);
   ImageJoueur();
   ImageBanque();
-  var ActualisationScore = document.getElementById("carte");
-  ActualisationScore.addEventListener("click",ScoreJoueur);
+  var BoutonReste = document.getElementById("reste");
+  BoutonReste.addEventListener("click",ajoutImgBanque);
 }
 
 /* Choisi un indice aleatoire d'une carte dans la liste tab
@@ -17,16 +19,31 @@ var getRandomIntInclusive = function (min, max) {
 }
 
 var ImageJoueur = function() {
-  N1 = getRandomIntInclusive(0,53)
-  image1=document.getElementById("image1")
-  image1.src=tab[N1];
+  N1 = getRandomIntInclusive(0,53);
+  image1=document.getElementById("image1");
+  image1.src=tab[N1]
+  carteJoueur.push(tab[N1])
+  ini = tab[N1] //tout ceci n'est pas tres optimise hehe
+  var sep_ini=ini.split('.');
+  x_ini=sep_ini[0]
+  valeur_ini = x_ini%13
+  score_joueur = score_joueur + valeur_ini;
+  panneau_score = document.getElementById("point_joueur");
+  panneau_score.innerHTML=score_joueur;
 }
 
 
 var ImageBanque = function() {
-  N2 = getRandomIntInclusive(0,53)
-  image2=document.getElementById("image2")
+  N2 = getRandomIntInclusive(0,53);
+  image2=document.getElementById("image2");
   image2.src=tab[N2];
+  ini_banque = tab[N2] //tout ceci n'est pas tres optimise non plus hehe
+  var sep_ini_banque=ini_banque.split('.');
+  x_ini_banque=sep_ini_banque[0]
+  valeur_ini_banque = x_ini_banque%13
+  score_banque = score_banque+ valeur_ini_banque;
+  panneau_score_banque = document.getElementById("point_banque");
+  panneau_score_banque.innerHTML=score_banque;
 }
 
 /* Lorsque le joueur clik sur "carte", creation d'une nouvelle image
@@ -39,24 +56,54 @@ var creerImg = function (chemin) {
 }
 
 var ajoutImgDansDiv = function () {
-    N3 = getRandomIntInclusive(0,53)
-    var chemin = tab[N3]
+    N3 = getRandomIntInclusive(0,53);
+    var chemin = tab[N3];
     var newImg = creerImg(chemin);
 	var divJS = document.getElementById('maDiv');
     divJS.appendChild(newImg);
+    ChangementScore(chemin);
 }
 
+var ChangementScore = function(chemin){
+    carteJoueur.push(chemin);
+    score_joueur = score_joueur + RecupValeurCarte(chemin);
+    panneau_score = document.getElementById("point_joueur");
+    panneau_score.innerHTML=score_joueur;
+    if (score_joueur > 21 ) { //ca ne marche pas
+      var perdu = "PERDU"
+      console.log(perdu);
+    }
+}
 
-/* Je n'ai aucune idee de comment faire
+/* calcul des points de la carte
 */
-var ScoreJoueur = function() {
-
+var RecupValeurCarte = function(chemin) {
+  var sep=chemin.split('.');
+  x=sep[0]
+  valeur = x%13
+  if (valeur == 11 || valeur == 12 || valeur == 0 ) {
+    return 10
+  }
+  else {
+    return valeur
+  }
 }
+
+var ajoutImgBanque = function(chemin){
+  N4 = getRandomIntInclusive(0,53);
+  var chemin = tab[N4];
+  var newImg = creerImg(chemin);
+  var divJS_banque = document.getElementById('maBanq');
+    divJS_banque.appendChild(newImg);
+    ChangementScore(chemin);
+}
+
 
 tab=["01.BMP","02.BMP","03.BMP","04.BMP","05.BMP","06.BMP","07.BMP","08.BMP","09.BMP","10.BMP","11.BMP","12.BMP","13.BMP","14.BMP","16.BMP","17.BMP","18.BMP","19.BMP","20.BMP","21.BMP","22.BMP","23.BMP","24.BMP","25.BMP",
 "26.BMP","27.BMP","28.BMP","29.BMP","30.BMP","31.BMP","32.BMP","33.BMP","34.BMP","35.BMP","36.BMP","37.BMP","38.BMP","39.BMP","40.BMP","41.BMP","42.BMP","43.BMP","44.BMP","45.BMP","46.BMP","47.BMP","48.BMP","49.BMP","50.BMP",
 "51.BMP","52.BMP","53.BMP","54.BMP"]
 
-score=[]
+val = []
+carteJoueur=[]
 
 window.addEventListener("load",setupListeners);
